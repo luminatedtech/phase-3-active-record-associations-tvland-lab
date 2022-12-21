70c89463050b683fa3decd769812003db13f588b
Allen <avalmercafe02@gmail.com>
2022-12-20 23:21:58 -0500

    commit tree efdb1055d7d972d7cb9e345e3ee38b59de9317cb

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  def build_network(network_id)
+    Show.first.network_id
+  end 
+  def actors_list
+    "#{self.actors.first.first_name} #{self.actors.first.last_name}"
+  end 
 end
